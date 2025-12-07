90b12428facb891d551aea0d222bc189f7037dac
Casey Toll <caseytoll78@gmail.com>
2025-12-07 20:06:06 +1100

    Style: Align schedule text sizes and shrink status badge - v872

--- Code.js
+++ Code.js
@@ -58,7 +58,7 @@ function doGet(e) {
 
     // Cache buster - update this to force client refresh
     // NOTE: bump this on each production deploy so that clients bust cached JS/CSS
-    template.appVersion = '871';
+    template.appVersion = '872';
 
     var result = template.evaluate()
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
