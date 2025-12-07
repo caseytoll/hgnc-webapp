1a776f7804e567dda2433b3639c353ce6dbd500d
Casey Toll <caseytoll78@gmail.com>
2025-12-07 12:12:32 +1100

    Fix: Add inline styles to force loading overlay visibility on page load

--- Code.js
+++ Code.js
@@ -84,7 +84,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@8e14318';  // Updated to master; production releases pin to commit SHA
+var CDN_TAG = '@5df1846';  // Updated to master; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
