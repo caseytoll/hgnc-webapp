9a14b7d726f38d0fe8b92901ff1de4f06c1d5e74
Casey Toll <caseytoll78@gmail.com>
2025-12-07 20:48:45 +1100

    Replace game modal with full-screen editor

--- Code.js
+++ Code.js
@@ -58,7 +58,7 @@ function doGet(e) {
 
     // Cache buster - update this to force client refresh
     // NOTE: bump this on each production deploy so that clients bust cached JS/CSS
-    template.appVersion = '875';
+    template.appVersion = '876';
 
     var result = template.evaluate()
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
@@ -84,7 +84,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@0090468';  // Updated to master; production releases pin to commit SHA
+var CDN_TAG = '@dac95f0';  // Updated to master; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
