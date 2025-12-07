b86af53fc4953a38d9429ab5a586d89a8585487e
Casey Toll <caseytoll78@gmail.com>
2025-12-07 13:10:08 +1100

    Fix: BYE game rendering structure - correct layout for date/edit on line 1, opponent on line 2, bump to v869

--- Code.js
+++ Code.js
@@ -58,7 +58,7 @@ function doGet(e) {
 
     // Cache buster - update this to force client refresh
     // NOTE: bump this on each production deploy so that clients bust cached JS/CSS
-    template.appVersion = '864';
+    template.appVersion = '869';
 
     var result = template.evaluate()
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
