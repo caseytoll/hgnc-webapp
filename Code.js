cff3e717093194ff93c49c9fda1083104ac24b7f
Casey Toll <caseytoll78@gmail.com>
2025-12-08 07:32:04 +1100

    Force cache bust with v900

--- Code.js
+++ Code.js
@@ -58,7 +58,7 @@ function doGet(e) {
 
     // Cache buster - update this to force client refresh
     // NOTE: bump this on each production deploy so that clients bust cached JS/CSS
-    template.appVersion = '882';
+    template.appVersion = '900';
 
     var result = template.evaluate()
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
@@ -84,7 +84,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@7d22278';  // Updated to master; production releases pin to commit SHA
+var CDN_TAG = '@08c956e';  // Updated to master; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
