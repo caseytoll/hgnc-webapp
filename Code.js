2b8f9056b7e0cdcfa78d8df3c80a709587de3e5d
Casey Toll <caseytoll78@gmail.com>
2025-12-07 12:14:47 +1100

    chore: Bump app version to 852 to force cache invalidation

--- Code.js
+++ Code.js
@@ -58,7 +58,7 @@ function doGet(e) {
 
     // Cache buster - update this to force client refresh
     // NOTE: bump this on each production deploy so that clients bust cached JS/CSS
-    template.appVersion = '828';
+    template.appVersion = '852';
 
     var result = template.evaluate()
         .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
@@ -84,7 +84,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@5df1846';  // Updated to master; production releases pin to commit SHA
+var CDN_TAG = '@1a776f7';  // Updated to master; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
