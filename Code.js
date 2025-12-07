b746b2d7827171f63a4b03c96175f7d2f7361f40
Casey Toll <caseytoll78@gmail.com>
2025-12-07 11:42:43 +1100

    chore: Update config.sh to use latest deployment ID (v844)

--- Code.js
+++ Code.js
@@ -84,7 +84,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@de8174c';  // Updated to master; production releases pin to commit SHA
+var CDN_TAG = '@86dcdb8';  // Updated to master; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
