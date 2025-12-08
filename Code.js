3a3f5662f4f9df85d6484eb0befe838f5abd0ec5
Casey Toll <caseytoll78@gmail.com>
2025-12-09 09:48:45 +1100

    FIX: Update CDN commit to a6529bf which has all webp assets

--- Code.js
+++ Code.js
@@ -84,7 +84,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@18bb16b';  // Updated to master; production releases pin to commit SHA
+var CDN_TAG = '@a6529bf';  // Updated to current HEAD with all assets; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
