cccee190d5a5c616964bad8a47f06bb66a0ee727
Casey Toll <caseytoll78@gmail.com>
2025-12-10 11:18:22 +1100

    Remove debugging logs from player analysis view

--- Code.js
+++ Code.js
@@ -85,7 +85,7 @@ function doGet(e) {
  * NOTE: This value is automatically updated by deploy script during releases.
  * Current HEAD commit is used to pin CDN references at deploy time.
  */
-var CDN_TAG = '@bc62aaf';  // Updated to current HEAD with all assets; production releases pin to commit SHA
+var CDN_TAG = '@c2cb7b2';  // Updated to current HEAD with all assets; production releases pin to commit SHA
 var CDN_BASE = 'https://cdn.jsdelivr.net/gh/caseytoll/hgnc-webapp' + CDN_TAG + '/assets/';
 
 /**
