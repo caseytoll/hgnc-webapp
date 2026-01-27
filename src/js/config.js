4ec93e03ca168423c8fccb3928873481cb8a75eb
Casey Toll <caseytoll78@gmail.com>
2026-01-27 14:18:55 +1100

    Point production API to Apps Script deployment with ladderUrl support

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,8 @@
 export const API_CONFIG = {
   // Your current deployed Apps Script web app URL
   // This is used for all server calls (loading data, saving lineups, etc.)
-  baseUrl: 'https://script.google.com/macros/s/AKfycbyBxhOJDfNBZuZ65St-Qt3UmmeAD57M0Jr1Q0MsoKGbHFxzu8rIvarJOOnB4sLeJZ-V/exec',
+  // Use the Apps Script deployment that includes ladderUrl support
+  baseUrl: 'https://script.google.com/macros/s/AKfycbxl3nsI-Y3pChZp-ogLZiqtdFDrudHFyz72oFAb3XDiRdStt2bu6P8PeI2yCZ4GmsLd/exec',
 
   // Set to true to use mock data for offline development
   useMockData: false,
